2ed82ed875835950eb884c544a10d12e6158425c
Youri Wijnands <hey@iruoy.com>
2024-02-09 14:37:19 +0100

    Add demo ui with symfony ux live components

--- migrations/Version20240208210000.php
+++ migrations/Version20240208210000.php
@@ -20,14 +20,18 @@ final class Version20240208210000 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, post_url VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) DEFAULT NULL, comment LONGTEXT NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) DEFAULT NULL, comment LONGTEXT NOT NULL, created_at DATETIME NOT NULL, post_id INT NOT NULL, INDEX IDX_9474526C4B89032C (post_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, url VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C4B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C4B89032C');
         $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE post');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
--- src/Controller/Api/CommentController.php
+++ src/Controller/Api/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api;
 
 use App\Entity\Comment;
+use App\Entity\Post;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,19 +14,47 @@ use Symfony\Component\Routing\Attribute\Route;
 class CommentController extends AbstractController
 {
     #[Route('/api/comments', methods: ['GET', 'HEAD'])]
-    public function index(EntityManagerInterface $entityManager, #[MapQueryParameter] string $postUrl): JsonResponse
+    public function index(EntityManagerInterface $entityManager, #[MapQueryParameter] string $url): JsonResponse
     {
-        $comments = $entityManager->getRepository(Comment::class)->findByPostUrl($postUrl);
+        $comments = $entityManager->getRepository(Comment::class)->findByPostUrl($url);
 
-        return $this->json($comments);
+        $data = [];
+        foreach ($comments as $comment) {
+            $data[] = [
+                'name' => $comment->getName(),
+                'email' => $comment->getEmail(),
+                'comment' => $comment->getComment(),
+                'createdAt' => $comment->getCreatedAt(),
+            ];
+        }
+
+        return $this->json($data);
     }
 
     #[Route('/api/comments', methods: ['POST'])]
-    public function create(EntityManagerInterface $entityManager, #[MapRequestPayload] Comment $comment): JsonResponse
-    {
+    public function create(
+        EntityManagerInterface       $entityManager,
+        #[MapQueryParameter] string  $url,
+        #[MapRequestPayload] Comment $comment
+    ): JsonResponse {
+        $post = $this->getPost($entityManager, $url);
+        $post->addComment($comment);
+
         $entityManager->persist($comment);
         $entityManager->flush();
 
-        return $this->json($comment);
+        return $this->json(['success' => true]);
+    }
+
+    public function getPost(EntityManagerInterface $entityManager, string $url): ?Post
+    {
+        $post = $entityManager->getRepository(Post::class)->findOneByUrl($url);
+        if (!$post) {
+            $post = new Post();
+            $post->setUrl($url);
+            $entityManager->persist($post);
+        }
+
+        return $post;
     }
 }
